871bc0fdbc2324b0925640dcb89e5044771e72c6
leonshimizu <lmshimizu@gmail.com>
2025-11-05 11:32:29 +1000

    Implement tests for all CRUD actions

--- app/views/recipes/_recipe.json.jbuilder
+++ app/views/recipes/_recipe.json.jbuilder
@@ -1,6 +1,9 @@
 json.id recipe.id
 json.title recipe.title
+json.chef recipe.chef
 json.ingredients recipe.ingredients
 json.directions recipe.directions
+json.image_url recipe.image_url
 json.prep_time recipe.prep_time
-json.chef recipe.chef
+json.created_at recipe.created_at
+json.updated_at recipe.updated_at
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
-
+# minitest
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/recipes.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Recipe.count, data.count
+  end
+
+  test "show" do
+    get "/recipes/#{Recipe.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal [ "id", "title", "chef", "ingredients", "directions", "image_url", "prep_time", "created_at", "updated_at" ], data.keys
+  end
+
+  test "create" do
+    assert_difference "Recipe.count", 1 do
+      post "/recipes.json", params: { title: "Cake", chef: "Peter", image_url: "test.jpg", prep_time: 10, ingredients: "Batter", directions: "Bake" }
+      assert_response 200
+    end
+  end
+
+  test "update" do
+    recipe = Recipe.first
+    put "/recipes/#{recipe.id}.json", params: { title: "Updated title" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated title", data["title"]
+  end
+
+  test "destroy" do
+    assert_difference "Recipe.count", -1 do
+      delete "/recipes/#{Recipe.first.id}.json"
+      assert_response 200
+    end
+  end
 end
