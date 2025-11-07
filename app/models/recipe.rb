893b42fea63ca8958c57dcf5bbc800be4bcc81f0
leonshimizu <lmshimizu@gmail.com>
2025-11-07 11:56:01 +1000

    Add recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
+  validates :title, presence: { message: "Email address cannot be blank." }
+  validates :prep_time, numericality: { greater_than: 0 }
+
+  validate :ingredients_and_directions_presence
+
   def ingredients_list
     ingredients.split(", ")
   end
@@ -17,5 +22,15 @@ class Recipe < ApplicationRecord
     result = ""
     result += "#{hours} hours " if hours > 0 # add the hours into the result string if hours isn't 0
     result += "#{minutes} minutes" if minutes > 0 # add the minutes into the result string if minutes isn't 0
+  end
+
+  private
+
+  def ingredients_and_directions_presence
+    if ingredients.blank? && directions.blank?
+      errors.add(:base, "ingredients and directions must be present")
+      # errors.add(:ingredients, "ingredients must be present")
+      # errors.add(:directions, "directions must be present")
+    end
   end
 end
